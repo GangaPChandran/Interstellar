8b0b601033a7b7cb07ea8e43f596c52758468b9a
Ganga P Chandran <gangapchandran12@gmail.com>
2020-12-16 00:59:39 +0530

    animated heading added

---
+++ main.js
@@ -0,0 +1,20 @@
+var button=document.querySelector("#btn");
+var txtInput=document.querySelector("#inputText");
+var outputDiv=document.querySelector("#outputText");
+
+var serverURL="https://api.funtranslations.com/translate/morse.json";
+
+function clickHandler(event)
+{
+   fetch(getTranslationURL(txtInput.value))
+     .then(response => response.json())
+     .then(json => {
+          outputDiv.innerText=json.contents.translated;
+         })
+     .catch(() =>  alert("Error struck,try again later"))
+}
+function getTranslationURL(input)
+{
+    return serverURL+"?"+"text="+input
+}
+button.addEventListener("click",clickHandler);
